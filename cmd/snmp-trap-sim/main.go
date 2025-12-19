c440f011092b32ca61de8a0cfa7d6a3dfc63479d
Aishwarya Gilhotra <aishwaryagilhotra@gmail.com>
2025-12-19 15:34:31 +0000

    Saving the SNMP v2c traps in a json file(for now)

--- cmd/snmp-trap-sim/main.go
+++ cmd/snmp-trap-sim/main.go
@@ -13,6 +13,7 @@ func main() {
 	addr := flag.String("addr", "localhost:5162", "UDP address")
 	device := flag.String("device", "router", "router|switch|firewall")
 	freq := flag.Int("freq", 3, "seconds between traps")
+	file := flag.String("file", "data/snmp-traps.json", "file to store traps")
 	flag.Parse()
 
 	rand.Seed(time.Now().UnixNano())
@@ -21,12 +22,21 @@ func main() {
 
 	for {
 		trap := snmptrap.RandomTrap(*device, "device-01")
+
+		// Send over UDP
 		err := snmptrap.SendTrap(*addr, trap)
 		if err != nil {
 			fmt.Println("failed to send trap:", err)
 		} else {
 			fmt.Println("sent trap:", trap.OID, "-", trap.Message)
 		}
+
+		// Save to JSON file
+		err = snmptrap.SaveTrapToFile(*file, trap)
+		if err != nil {
+			fmt.Println("failed to save trap:", err)
+		}
+
 		time.Sleep(time.Duration(*freq) * time.Second)
 	}
 }
