aec24661500894bdf34c5c2d3feec0d50dc94c87
Aishwarya Gilhotra <aishwaryagilhotra@gmail.com>
2026-02-06 06:12:05 +0000

    refactor: move UDP listener logic into package and keep single main entry point

--- sysylog-listener/udplisten.go
+++ sysylog-listener/udplisten.go
@@ -1,23 +1,33 @@
-package main
+package sysloglistener
 
 import (
 	"fmt"
 	"net"
 )
 
-func main() {
-	addr, _ := net.ResolveUDPAddr("udp", ":5140")
+// StartUDPListener starts a UDP server on the given address
+// and prints incoming syslog messages.
+func StartUDPListener(address string) error {
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		return err
+	}
+
 	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer conn.Close()
 
 	buf := make([]byte, 2048)
+	fmt.Printf("Listening on UDP %s ...\n", address)
 
-	fmt.Println("Listening on UDP :5140 ...")
 	for {
-		n, remoteAddr, _ := conn.ReadFromUDP(buf)
+		n, remoteAddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("read error:", err)
+			continue
+		}
 		fmt.Printf("From %s: %s\n", remoteAddr, string(buf[:n]))
 	}
 }
